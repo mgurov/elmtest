8060d3b0ddb23f07d549d807e9b75491c76768d5
Mykola Gurov <ngurov@gmail.com>
2020-09-20 21:58:13 +0200

    initial

---
+++ src/Main.elm
@@ -0,0 +1,68 @@
+module Main exposing (..)
+
+-- Press buttons to increment and decrement a counter.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/buttons.html
+--
+
+
+import Browser
+import Html exposing (Html, button, div, span, text, textarea)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (rows)
+import Html.Attributes exposing (cols)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { count: Int
+  , json: String}
+
+
+init : Model
+init =
+  Model 0 "{}"
+
+-- UPDATE
+
+
+type Msg
+  = Increment
+  | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      {model | count = model.count + 1}
+
+    Decrement ->
+      {model | count = model.count - 1}
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , span [] [ text (String.fromInt model.count) ]
+    , button [ onClick Increment ] [ text "+" ]
+    , div [] [ textarea [rows 20, cols  80] [] ]
+    
+    ]
