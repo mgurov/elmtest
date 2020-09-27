a791a9b0c183052bf42a821b81539eb5205ab3a6
Mykola Gurov <ngurov@gmail.com>
2020-09-27 19:44:03 +0200

    time

--- src/Main.elm
+++ src/Main.elm
@@ -8,10 +8,16 @@ module Main exposing (..)
 
 
 import Browser
-import Html exposing (Html, button, div, span, text, textarea)
+import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (rows)
 import Html.Attributes exposing (cols)
+
+import Json.Decode as Jdec
+import Json.Decode.Pipeline as Jpipe
+import Json.Encode as Jenc
+import Dict exposing (Dict, map, toList)
+import Array exposing (Array, map)
 
 
 
@@ -37,7 +43,7 @@ init =
   {
     "something": "in the air",
     
-    "tonight": "yeah,
+    "tonight": "yeah",
     "inval": 1
   }
   """
@@ -70,8 +76,56 @@ view model =
     [ button [ onClick Decrement ] [ text "-" ]
     , span [] [ text (String.fromInt model.count) ]
     , button [ onClick Increment ] [ text "+" ]
-    , div [] [ textarea [rows 20, cols  80] [text model.json] ]
+    , div [] [ 
+      textarea [rows 20, cols  40] [text model.json] 
+      , pre [] [text (welcome model.json)]
+    ]
     
     ]
 
     -- TODO: json pretty formatting
+
+--  JSON decoding
+
+
+type alias Welcome =
+    { something : String
+    , tonight : String
+    , inval : Int
+    }
+
+-- decoders and encoders
+
+-- welcomeToString : Welcome -> String
+-- welcomeToString r = Jenc.encode 0 (encodeWelcome r)
+
+welcome : Jdec.Decoder Welcome
+welcome =
+    Jdec.succeed Welcome
+        |> Jpipe.required "something" Jdec.string
+        |> Jpipe.required "tonight" Jdec.string
+        |> Jpipe.required "inval" Jdec.int
+
+-- encodeWelcome : Welcome -> Jenc.Value
+-- encodeWelcome x =
+--     Jenc.object
+--         [ ("something", Jenc.string x.something)
+--         , ("tonight", Jenc.string x.tonight)
+--         , ("inval", Jenc.int x.inval)
+--         ]
+
+-- --- encoder helpers
+
+-- makeArrayEncoder : (a -> Jenc.Value) -> Array a -> Jenc.Value
+-- makeArrayEncoder f arr =
+--     Jenc.array (Array.map f arr)
+
+-- makeDictEncoder : (a -> Jenc.Value) -> Dict String a -> Jenc.Value
+-- makeDictEncoder f dict =
+--     Jenc.object (toList (Dict.map (\k -> f) dict))
+
+-- makeNullableEncoder : (a -> Jenc.Value) -> Maybe a -> Jenc.Value
+-- makeNullableEncoder f m =
+--     case m of
+--     Just x -> f x
+--     Nothing -> Jenc.null
---
+++ src/Time/Timer.elm
@@ -0,0 +1,117 @@
+module Time.Timer exposing (..)
+
+-- Show the current time in your time zone.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/effects/time.html
+--
+-- For an analog clock, check out this SVG example:
+--   https://elm-lang.org/examples/clock
+--
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Task
+import Time
+import Json.Decode exposing (string)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { zone : Time.Zone
+  , time : Time.Posix
+  , color: String
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Time.utc (Time.millisToPosix 0) "red"
+  , Task.perform AdjustTimeZone Time.here
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time.Posix
+  | AdjustTimeZone Time.Zone
+  | SetColor String
+
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      ( { model | time = newTime }
+      , Cmd.none
+      )
+
+    AdjustTimeZone newZone ->
+      ( { model | zone = newZone }
+      , Cmd.none
+      )
+
+    SetColor color -> 
+      ( { model | color = color }
+      , Cmd.none
+      )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 1000 Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  let
+    hour   = String.fromInt (Time.toHour   model.zone model.time)
+    minute = String.fromInt (Time.toMinute model.zone model.time)
+    second = String.fromInt (Time.toSecond model.zone model.time)
+  in
+  div [] [
+    h1 [style "color" model.color] [
+      text (hour ++ ":" ++ minute ++ ":" ++ second)
+    ]
+    , div [] [
+      setColorButton "green"
+      ,setColorButton "red"
+      ,setColorButton "grey"
+    ]
+  ]
+
+-- view hjelpers
+setColorButton: String -> Html Msg
+setColorButton color = 
+  button [onClick (SetColor color)] [text(color)]
+    
